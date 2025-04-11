4a4c00505d246f2b98d70ba83a8e368a109720af
KanwardeepKaur <ikanwardeep30@gmail.com>
2025-04-11 19:06:23 -0400

    added migration and Deals index, create modified

--- Migrations/20250411222326_Deals.Designer.cs
+++ Migrations/20250411222326_Deals.Designer.cs
@@ -4,6 +4,7 @@ using Lab5;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lab5.Migrations
 {
     [DbContext(typeof(DealsFinderDbContext))]
-    partial class DealsFinderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250411222326_Deals")]
+    partial class Deals
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250411222326_Deals.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab5.Migrations
+{
+    /// <inheritdoc />
+    public partial class Deals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DealId",
+                table: "FoodDeliveryService",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Deal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FoodDeliveryServiceId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deal", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FoodDeliveryService_DealId",
+                table: "FoodDeliveryService",
+                column: "DealId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FoodDeliveryService_Deal_DealId",
+                table: "FoodDeliveryService",
+                column: "DealId",
+                principalTable: "Deal",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FoodDeliveryService_Deal_DealId",
+                table: "FoodDeliveryService");
+
+            migrationBuilder.DropTable(
+                name: "Deal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FoodDeliveryService_DealId",
+                table: "FoodDeliveryService");
+
+            migrationBuilder.DropColumn(
+                name: "DealId",
+                table: "FoodDeliveryService");
+        }
+    }
+}
